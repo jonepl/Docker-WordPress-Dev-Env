dbdca79a20d02073df0ad5503e900724076c62c8
Purnell Jones <plj.coding@gmail.com>
2020-03-30 12:20:47 -0400

    Intermediate commit.

--- WordPress/db/migrate.sql
+++
@@ -1,5 +0,0 @@
-/* This file is used to change Website address when moving from one environment to another */
--- UPDATE wp_options SET option_value = replace(option_value, 'http://www.oldurl', 'http://www.newurl') WHERE option_name = 'home' OR option_name = 'siteurl';
--- UPDATE wp_posts SET guid = replace(guid, 'http://www.oldurl','http://www.newurl');
--- UPDATE wp_posts SET post_content = replace(post_content, 'http://www.oldurl', 'http://www.newurl');
--- UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://www.oldurl','http://www.newurl');
